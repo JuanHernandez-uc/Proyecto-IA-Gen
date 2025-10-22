47d4b7b26d03c558e19c90b4b3f00ba0dccb77e2
Juan Manuel Hernández Roda <juan_manuel1402@uc.cl>
2025-10-22 14:21:15 -0300

    add datasets

--- WikiData/README.md
+++ WikiData/README.md
@@ -1,1 +1,16 @@
 # Scripts para extraer información de WikiData
+
+## IMPORTANTE
+
+Para correr el código hay que cambiar el identificador "QXXX" en la línea 28. Los identificadores de los países se pueden obtener en el archivo paises.txt
+
+Además, hay que cambiar en la línea 237 el nombre del dataset que se producirá.
+
+## Otros
+
+Desde la línea 252 en adelante está lo importante del código:
+- El primer argumento de la función `query` es el LIMIT, que es cuántas entidades se procesan por batch.
+- El segundo argumento de la función `query` es el OFFSET, que es cuántas lineas se omite la consulta (a veces las respuestas de la query son deterministas, entonces el offset sirve para evitar las primeras filas)
+
+Si se cae, hay que cambiar el inicio del for i in range (por lo que se explicaba del OFFSET)
+
--- WikiData/wikidata_extractor.py
+++ WikiData/wikidata_extractor.py
@@ -1,230 +1,263 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 import pandas as pd
+import os
+import time
+from tqdm import tqdm
 
-# Conexión con WikiData
-sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
+def query(limit, offset):
+    # Conexión con WikiData
+    sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
-# Query
-sparql.setQuery("""
-SELECT  
-    ?persona ?personaLabel 
-    ?lugarNacimiento ?lugarNacimientoLabel 
-    ?ocupacion ?ocupacionLabel 
-    ?premio ?premioLabel 
-    ?fechaNac ?fechaDef
-    ?ciudadania ?ciudadaniaLabel
-    ?educadoEn ?educadoEnLabel
-    ?empleador ?empleadorLabel
-    ?obraNotable ?obraNotableLabel
-    ?miembroDe ?miembroDeLabel
-    ?cargo ?cargoLabel
-WHERE {
-  {
-    SELECT DISTINCT ?persona WHERE {
-      ?persona wdt:P31 wd:Q5;   # Instancia de ser humano
-               wdt:P27 wd:Q96.  # Con ciudadanía mexicana
-    }
-    LIMIT 10
-  }
-
-  # --- Relaciones opcionales de cada persona ---
-  OPTIONAL { ?persona wdt:P19  ?lugarNacimiento. }   # Lugar de nacimiento
-  OPTIONAL { ?persona wdt:P106 ?ocupacion. }          # Ocupación
-  OPTIONAL { ?persona wdt:P166 ?premio. }             # Premio
-  OPTIONAL { ?persona wdt:P569 ?fechaNac. }           # Fecha de nacimiento
-  OPTIONAL { ?persona wdt:P570 ?fechaDef. }           # Fecha de defunción
-  OPTIONAL { ?persona wdt:P27  ?ciudadania. }         # Ciudadanía
-  OPTIONAL { ?persona wdt:P69  ?educadoEn. }          # Educado en
-  OPTIONAL { ?persona wdt:P108 ?empleador. }          # Empleador
-  OPTIONAL { ?persona wdt:P800 ?obraNotable. }        # Obra notable
-  OPTIONAL { ?persona wdt:P463 ?miembroDe. }          # Miembro de
-  OPTIONAL { ?persona wdt:P39  ?cargo. }              # Cargo
-
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "es,en". }
-}
-""")
-
-# Formato JSON para hacer la request
-sparql.setReturnFormat(JSON)
-
-print("Realizando request")
-
-# Ejecuta la consulta
-results = sparql.query().convert()
-
-print("Request realizada")
-
-# Convertirlo en DataFrame
-df = pd.json_normalize(results["results"]["bindings"])
-
-# Columnas consultadas
-names_mapping = {
-    "lugarNacimientoLabel.value": "Lugar de nacimiento",
-    "ocupacionLabel.value": "Ocupación",
-    "ciudadaniaLabel.value": "Ciudadanía",
-    "educadoEnLabel.value": "Educado en",
-    "empleadorLabel.value": "Empleador",
-    "obraNotableLabel.value": "Obra notable",
-    "miembroDeLabel.value": "Miembro de",
-    "cargoLabel.value": "Cargo",
-}
-optional_names_mapping = {
-    "premioLabel.value": "Premio recibido"
-}
-date_mapping = {  # literales (sin label)
-    "fechaNac.value": "Fecha de nacimiento",
-    "fechaDef.value": "Fecha de defunción",
-}
-
-# Crear tripletas
-triplets = []
-
-# Se verifica que exista (la relación podría no existir). En el caso de las fechas no hay label porque son literales
-for _, row in df.iterrows():
-    entidad = row['personaLabel.value']
-
-    for label, name in names_mapping.items():
-        if label in row:
-            triplets.append([entidad, name, row[label], row[label.replace("Label.value", ".value")]])
+    query_string = """
+    SELECT  
+        ?persona ?personaLabel 
+        ?lugarNacimiento ?lugarNacimientoLabel 
+        ?ocupacion ?ocupacionLabel 
+        ?premio ?premioLabel 
+        ?fechaNac ?fechaDef
+        ?ciudadania ?ciudadaniaLabel
+        ?educadoEn ?educadoEnLabel
+        ?empleador ?empleadorLabel
+        ?obraNotable ?obraNotableLabel
+        ?miembroDe ?miembroDeLabel
+        ?cargo ?cargoLabel
+    WHERE {
+    {
+        SELECT DISTINCT ?persona WHERE {
+        ?persona wdt:P31 wd:Q5;   # Instancia de ser humano
+                wdt:P27 wd:Q783.  # Filtra por la ciudadanía (país al que pertenece la entidad)
+        } 
+    """
     
-    for label, name in optional_names_mapping.items():
-        if label in row and str(row.get(label)) != "nan":
-            triplets.append([entidad, name, row[label], row[label.replace("Label.value", ".value")]])
+    query_string += f"LIMIT {limit} OFFSET {offset}" + "}"
 
-    for date_key, name in date_mapping.items():
-        value = row.get(date_key)
-        if isinstance(value, str):
-            obj = value.split("T")[0]
-            triplets.append([entidad, name, obj, None])
+    query_string += """
+    # --- Relaciones opcionales de cada persona ---
+    OPTIONAL { ?persona wdt:P19  ?lugarNacimiento. }   # Lugar de nacimiento
+    OPTIONAL { ?persona wdt:P106 ?ocupacion. }          # Ocupación
+    OPTIONAL { ?persona wdt:P166 ?premio. }             # Premio
+    OPTIONAL { ?persona wdt:P569 ?fechaNac. }           # Fecha de nacimiento
+    OPTIONAL { ?persona wdt:P570 ?fechaDef. }           # Fecha de defunción
+    OPTIONAL { ?persona wdt:P27  ?ciudadania. }         # Ciudadanía
+    OPTIONAL { ?persona wdt:P69  ?educadoEn. }          # Educado en
+    OPTIONAL { ?persona wdt:P108 ?empleador. }          # Empleador
+    OPTIONAL { ?persona wdt:P800 ?obraNotable. }        # Obra notable
+    OPTIONAL { ?persona wdt:P463 ?miembroDe. }          # Miembro de
+    OPTIONAL { ?persona wdt:P39  ?cargo. }              # Cargo
 
-# Lo convertimos en dataframe y sacamos duplicados
-df_triplets = pd.DataFrame(triplets, columns=["entidad", "relacion", "objeto", "uri_object"]).drop_duplicates()
+    SERVICE wikibase:label { bd:serviceParam wikibase:language "es,en". }
+    }
+    """
 
-# Función para explorar en profundidad. Con profundidad 1 solo se hace la consulta anterior, con profundidad > 2 empieza a explorar.
-# Solo explora considerando dos objetos por cada entidad obtenida, para que no explote jjsdjsdj
-def expand_level(qids, depth=1, extension_limit = 2):
-    all_triplets = []
+    # Query
+    sparql.setQuery(query_string)
 
-    frontier = qids
+    # Formato JSON para hacer la request
+    sparql.setReturnFormat(JSON)
 
-    for i in range(2, depth + 1):
-        print("Ejecutando profundidad:", i)
-        # Caso en que no tengamos más entidades por explorar
-        if not frontier:
-            break
+    print("Realizando request")
+
+    # Ejecuta la consulta
+    results = sparql.query().convert()
+
+    print("Request realizada")
+
+    # Convertirlo en DataFrame
+    df = pd.json_normalize(results["results"]["bindings"])
+
+    # Columnas consultadas
+    names_mapping = {
+        "lugarNacimientoLabel.value": "Lugar de nacimiento",
+        "ocupacionLabel.value": "Ocupación",
+        "ciudadaniaLabel.value": "Ciudadanía",
+        "educadoEnLabel.value": "Educado en",
+        "empleadorLabel.value": "Empleador",
+        "obraNotableLabel.value": "Obra notable",
+        "miembroDeLabel.value": "Miembro de",
+        "cargoLabel.value": "Cargo",
+    }
+    optional_names_mapping = {
+        "premioLabel.value": "Premio recibido"
+    }
+    date_mapping = {  # literales (sin label)
+        "fechaNac.value": "Fecha de nacimiento",
+        "fechaDef.value": "Fecha de defunción",
+    }
+
+    # Crear tripletas
+    triplets = []
+
+    # Se verifica que exista (la relación podría no existir). En el caso de las fechas no hay label porque son literales
+    for _, row in df.iterrows():
+        entidad = row['personaLabel.value']
+
+        for label, name in names_mapping.items():
+            if label in row and str(row.get(label)) != "nan":
+                triplets.append([entidad, name, row[label], row[label.replace("Label.value", ".value")]])
         
-        # Dataframe en el que guardaremos la información de cada objeto
-        df2_parts = []
+        for label, name in optional_names_mapping.items():
+            if label in row and str(row.get(label)) != "nan":
+                triplets.append([entidad, name, row[label], row[label.replace("Label.value", ".value")]])
 
-        # Consultamos máximo 5 relaciones posibles por objeto
-        for s_uri in frontier:
-            query = f"""
-            PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-            PREFIX wd: <http://www.wikidata.org/entity/>
-            PREFIX wikibase: <http://wikiba.se/ontology#>
+        for date_key, name in date_mapping.items():
+            value = row.get(date_key)
+            if isinstance(value, str):
+                obj = value.split("T")[0]
+                triplets.append([entidad, name, obj, None])
 
-            SELECT ?s ?sLabel ?p ?prop ?propLabel ?o ?oLabel WHERE {{
-            BIND(<{s_uri}> AS ?s)
+    # Lo convertimos en dataframe y sacamos duplicados
+    df_triplets = pd.DataFrame(triplets, columns=["entidad", "relacion", "objeto", "uri_object"]).drop_duplicates()
 
-            ?s ?p ?o .
-            FILTER(STRSTARTS(STR(?p), STR(wdt:)))
-            FILTER(STRSTARTS(STR(?o), STR(wd:Q)))
+    # Función para explorar en profundidad. Con profundidad 1 solo se hace la consulta anterior, con profundidad > 2 empieza a explorar.
+    # Solo explora considerando dos objetos por cada entidad obtenida, para que no explote jjsdjsdj
+    def expand_level(qids, depth=1, extension_limit = 5):
+        all_triplets = []
 
-            # mapear wdt:Pxx -> wd:Pxx (entidad propiedad) para obtener label
-            ?prop wikibase:directClaim ?p .
+        frontier = qids
 
-            SERVICE wikibase:label {{ bd:serviceParam wikibase:language "es". }}
-            }}
-            LIMIT 5
-            """
+        for i in range(2, depth + 1):
+            print("Ejecutando profundidad:", i)
+            # Caso en que no tengamos más entidades por explorar
+            if not frontier:
+                break
+            
+            # Dataframe en el que guardaremos la información de cada objeto
+            df2_parts = []
 
-            # Ejecutar
-            s2 = SPARQLWrapper("https://query.wikidata.org/sparql")
-            s2.setQuery(query)
-            s2.setReturnFormat(JSON)
-            res = s2.query().convert()
-            df2_one = pd.json_normalize(res["results"]["bindings"])
+            # Consultamos máximo 5 relaciones posibles por objeto
+            for s_uri in tqdm(frontier):
+                query = f"""
+                PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+                PREFIX wd: <http://www.wikidata.org/entity/>
+                PREFIX wikibase: <http://wikiba.se/ontology#>
 
-            if not df2_one.empty:
-                df2_parts.append(df2_one)
+                SELECT ?s ?sLabel ?p ?prop ?propLabel ?o ?oLabel WHERE {{
+                BIND(<{s_uri}> AS ?s)
 
-        # Concatenamos los resultados de todos los objetos        
-        df2 = pd.concat(df2_parts, ignore_index=True) if df2_parts else pd.DataFrame(columns=[
-            "s.value","sLabel.value","p.value","prop.value","propLabel.value","o.value","oLabel.value"
-        ])
+                ?s ?p ?o .
+                FILTER(STRSTARTS(STR(?p), STR(wdt:)))
+                FILTER(STRSTARTS(STR(?o), STR(wd:Q)))
 
-        # Construir tripletas de este nivel
-        lvl_triplets = []
-        next_frontier = []
+                # mapear wdt:Pxx -> wd:Pxx (entidad propiedad) para obtener label
+                ?prop wikibase:directClaim ?p .
 
-        # Se sigue la misma lógica anterior, verificado que exista.
-        # Además, hay un filtro adicional para valores "Q123133" que la consulta los devuelve cuando no los encuentra en español
-        for _, row in df2.iterrows():
-            s_label = row.get("sLabel.value")
-            p_label = row.get("propLabel.value")
-            o_label = row.get("oLabel.value")
+                SERVICE wikibase:label {{ bd:serviceParam wikibase:language "es". }}
+                }}
+                LIMIT 5
+                """
 
-            if s_label and p_label:
-                #print(row)
-                if not(o_label[0] == "Q" and o_label[1:-1]) and not(s_label[0] == "Q" and s_label[1:-1]):
-                    lvl_triplets.append([s_label, p_label, o_label])
+                # Ejecutar
+                s2 = SPARQLWrapper("https://query.wikidata.org/sparql")
+                s2.setQuery(query)
+                s2.setReturnFormat(JSON)
+                res = s2.query().convert()
+                df2_one = pd.json_normalize(res["results"]["bindings"])
 
-            o_uri = str(row.get("o.value", ""))
-            if o_uri.startswith("http://www.wikidata.org/entity/"):
-                next_frontier.append([s_label, o_uri])
+                if not df2_one.empty:
+                    df2_parts.append(df2_one)
 
-        # Filtramos los duplicados
-        df_lvl = (
-            pd.DataFrame(lvl_triplets, columns=["entidad", "relacion", "objeto"])
-            .drop_duplicates()
-        )
-        all_triplets.append(df_lvl)
+                time.sleep(0.1)
 
-        print("Profundidad", i, "lista\n")
+            # Concatenamos los resultados de todos los objetos        
+            df2 = pd.concat(df2_parts, ignore_index=True) if df2_parts else pd.DataFrame(columns=[
+                "s.value","sLabel.value","p.value","prop.value","propLabel.value","o.value","oLabel.value"
+            ])
 
-        # Siguiente profundidad, limitamos a extension_limit objetos por entidad
-        frontier = []
-        count = {}
+            # Construir tripletas de este nivel
+            lvl_triplets = []
+            next_frontier = []
 
-        for entity, o_uri in next_frontier:
-            if entity not in count:
-                count[entity] = 1
-            else:
-                count[entity] += 1
+            # Se sigue la misma lógica anterior, verificado que exista.
+            # Además, hay un filtro adicional para valores "Q123133" que la consulta los devuelve cuando no los encuentra en español
+            for _, row in df2.iterrows():
+                s_label = row.get("sLabel.value")
+                p_label = row.get("propLabel.value")
+                o_label = row.get("oLabel.value")
 
-            if count[entity] <= extension_limit:
-                frontier.append(o_uri)
+                if s_label and p_label:
+                    #print(row)
+                    if not(o_label[0] == "Q" and o_label[1:-1]) and not(s_label[0] == "Q" and s_label[1:-1]):
+                        lvl_triplets.append([s_label, p_label, o_label])
 
-    return pd.concat(all_triplets, ignore_index=True).drop_duplicates()
+                o_uri = str(row.get("o.value", ""))
+                if o_uri.startswith("http://www.wikidata.org/entity/"):
+                    next_frontier.append([s_label, o_uri])
 
-# Entidades a expandir, consideramos máximo 2 objetos por entidad
-count = {}
-qids_lvl_1 = []
+            # Filtramos los duplicados
+            df_lvl = (
+                pd.DataFrame(lvl_triplets, columns=["entidad", "relacion", "objeto"])
+                .drop_duplicates()
+            )
+            all_triplets.append(df_lvl)
 
-for _, row in df_triplets.iterrows():
-    entity = row["entidad"]
-    if entity not in count:
-        count[entity] = 1
+            print("Profundidad", i, "lista\n")
+
+            # Siguiente profundidad, limitamos a extension_limit objetos por entidad
+            frontier = []
+            count = {}
+
+            for entity, o_uri in next_frontier:
+                if entity not in count:
+                    count[entity] = 1
+                else:
+                    count[entity] += 1
+
+                if count[entity] <= extension_limit:
+                    frontier.append(o_uri)
+
+        return pd.concat(all_triplets, ignore_index=True).drop_duplicates()
+
+    # Entidades a expandir, consideramos máximo 2 objetos por entidad
+    count = {}
+    qids_lvl_1 = []
+
+    for _, row in df_triplets.iterrows():
+        entity = row["entidad"]
+        if entity not in count:
+            count[entity] = 1
+        else:
+            count[entity] += 1
+
+        if count[entity] <= 5:
+            qids_lvl_1.append(row["uri_object"])
+
+    # Llamamos a la función para expandir
+    df_lvl_2 = expand_level(qids_lvl_1, depth=2)
+
+    # Dropeamos los links
+    df_triplets = df_triplets.drop(["uri_object"], axis=1)
+
+    # Dropeamos los duplicados (solo para confirmar)
+    df_all = pd.concat([df_triplets, df_lvl_2], ignore_index=True).drop_duplicates()
+    df_all = (
+        df_all
+        .drop_duplicates(subset=["entidad","relacion","objeto"], keep="first")
+    )
+
+    file_path = "Datasets/honduras.csv"
+
+    if os.path.exists(file_path):
+        df_prev = pd.read_csv(file_path)
+        df_all = pd.concat([df_prev, df_all], ignore_index=True)
+        df_all = df_all.drop_duplicates(subset=["entidad", "relacion", "objeto"])
+        print(f"Archivo existente: {len(df_prev)} filas → Nuevo total: {len(df_all)} filas.")
     else:
-        count[entity] += 1
+        print("Creando nuevo archivo.")
 
-    if count[entity] <= 2:
-        qids_lvl_1.append(row["uri_object"])
+    # Escribimos en el archivo
+    df_all.to_csv(file_path, index=False, encoding="utf-8")
 
-# Llamamos a la función para expandir
-df_lvl_2 = expand_level(qids_lvl_1, depth=2)
+    print("Archivo guardado")
 
-# Dropeamos los links
-df_triplets = df_triplets.drop(["uri_object"], axis=1)
+entidades = 20
 
-# Dropeamos los duplicados (solo para confirmar)
-df_all = pd.concat([df_triplets, df_lvl_2], ignore_index=True).drop_duplicates()
-df_all = (
-    df_all
-    .drop_duplicates(subset=["entidad","relacion","objeto"], keep="first")
-)
+j = 1
 
-# Convertimos a dataset
-df_all.to_csv("tripletas_mexico.csv", index=False, encoding="utf-8")
-
-print("Archivo guardado")
+for i in range(820, 1200, entidades):
+    print("Ejecución:", j)
+    try:
+        query(entidades, i)
+        j += 1
+    except:
+        query(entidades, i)
+        j += 1
